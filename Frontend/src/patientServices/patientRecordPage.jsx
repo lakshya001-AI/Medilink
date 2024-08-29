71c13320a5f9dcbf37cf24b94cdbdfb60f7efcc3
Lakshya Makode <makodelakshya101@gmail.com>
2024-08-29 22:32:34 +0530

    commit changes

--- Frontend/src/patientServices/patientRecordPage.jsx
+++ Frontend/src/patientServices/patientRecordPage.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import Style from "../App.module.css";
 
 function PatientRecordPage() {
-  const [records, setRecords] = useState([]);
+
+  const [records , setRecords] = useState([]);
 
   return (
     <>
@@ -35,24 +36,30 @@ function PatientRecordPage() {
         {/* ----------- section 1 ---------------- */}
 
         <div className={Style.section1MedicalRecordPatient}>
-          <div className={Style.section1MRP1}>
-            {records.length === 0 ? (
-              <div className={Style.noRecords}>
-                {/* Render this when records length is 0 */}
-                <p>No records found.</p>
-              </div>
-            ) : (
-              <div className={Style.recordsList}>
-                {/* Render this when records length is not 0 */}
-                {records.map((record, index) => (
-                  <p key={index}>{record.name}</p>
-                ))}
-              </div>
-            )}
+
+           <div className={Style.section1MRP1}>
+        {records.length === 0 ? (
+          <div className={Style.noRecords}>
+            {/* Render this when records length is 0 */}
+            <p>No records found.</p>
+          </div>
+        ) : (
+          <div className={Style.recordsList}>
+            {/* Render this when records length is not 0 */}
+            {records.map((record, index) => (
+              <p key={index}>{record.name}</p> 
+            ))}
+          </div>
+        )}
+      </div>
+
+          <div className={Style.section1MRP2}>
+
           </div>
 
-          <div className={Style.section1MRP2}></div>
         </div>
+
+
       </div>
     </>
   );
